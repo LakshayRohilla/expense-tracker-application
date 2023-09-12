62a8171ca40e210326ef68de0013ad355e057c5b
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-09-12 19:32:03 +0530

    Added card component

---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import './Expenses.css';
+
+export default function Expenses({items}){
+    return (
+        <Card className='expenses'>
+            {items.map(item =>
+                <div key={item.id}>
+                    <ExpenseItem title={item.title} amount={item.amount} date={item.date}/>
+                </div>
+            )}
+        </Card>
+    )
+}
