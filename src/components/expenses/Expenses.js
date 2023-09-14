d24af8b65936edd254d3ecdeb04da241bb5dfbbd
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-09-14 13:36:43 +0530

    Outputing conditional content which is the list of exp

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -16,15 +16,39 @@ export default function Expenses({items}) {
         return item.date.getFullYear().toString() === filteredYear;
     });
 
+    // Another way for conditional rendering.
+
+    // let expensesContent = <p>No expenses found.</p>;
+
+    // if (filteredExpenses.length > 0) {
+    //   expensesContent = filteredExpenses.map((expense) => (
+    //     <ExpenseItem
+    //       key={expense.id}
+    //       title={expense.title}
+    //       amount={expense.amount}
+    //       date={expense.date}
+    //     />
+    //   ));
+    // }
+
     return (
         <Card className='expenses'>
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {filteredExpenses.map(item =>
+            {/*{expensesContent}*/}
+            {filteredExpenses.length === 0 ? <p>No data available !!</p> : filteredExpenses.map(item =>
                 // <div key={item.id}>
                 //     <ExpenseItem title={item.title} amount={item.amount} date={item.date}/>
                 // </div>
                 <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}/>
-            )}
+            ) }
+
+            {/*Code without conditional rendering*/}
+            {/*{filteredExpenses.map(item =>*/}
+            {/*    // <div key={item.id}>*/}
+            {/*    //     <ExpenseItem title={item.title} amount={item.amount} date={item.date}/>*/}
+            {/*    // </div>*/}
+            {/*    <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}/>*/}
+            {/*)}*/}
         </Card>
     )
 }
